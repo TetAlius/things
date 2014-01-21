e7cbb4864258b629f462aaf133abdd114bebf341
Marta Jiménez Franco <jimenez.franco.marta@gmail.com>
2014-01-21 13:51:42 +0100

    Change given location for correct location
    And the number of hours by the number of classes

---
+++ modifySheduleCSV.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+if (( $# != 3 )); then
+    	echo "Usage: input.csv output.csv "X" where X is the number of your seminar"
+  	exit 1
+fi
+
+location="\"Escuela Universitaria de Ingeniería Informática, Calle Valdés Salas, Oviedo, Spain\""
+hoursPerClass=2
+seminar=$(echo S.$3)
+
+while IFS=, read subject startDate startTime endDate endTime description class
+do
+	if [[ $subject == "Subject" ]]; then
+		 echo "$subject,$startDate,$startTime,$endDate,$endTime,$description,$class" >> $2
+	 else
+	if [ -n "$subject" ]; then
+		arr=($description)
+		classHour=$(echo "${arr[4]}")
+		if [[ $subject == *$seminar* ]]; then
+			classNumber=$(echo $classHour)
+		else
+			classNumber=$(echo $(($classHour / $hoursPerClass))) 
+		fi
+		echo "$subject,$startDate,$startTime,$endDate,$endTime, Clase número $classNumber de $subject. Aula:$class,$location" >> $2	
+	fi
+fi
+done < $1
